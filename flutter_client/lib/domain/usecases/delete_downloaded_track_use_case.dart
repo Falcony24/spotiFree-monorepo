7666ae5d17bb716d52fb493340632475aa804636
Łukasz Sokołowski <79717920+Falcony24@users.noreply.github.com>
2026-04-27 15:20:01 +0200

    flutter-client/use caseses for downloadish actions added

---
+++ flutter_client/lib/domain/usecases/delete_downloaded_track_use_case.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:frontend/data/repositories/track_repository.dart';
+
+class DeleteDownloadedTrackUseCase {
+  final TrackRepository _repository;
+
+  DeleteDownloadedTrackUseCase({TrackRepository? repository})
+      : _repository = repository ?? TrackRepository();
+
+  Future<void> execute(String trackId, String filePath) async {
+    final file = File(filePath);
+    if (await file.exists()) await file.delete();
+    await _repository.removeDownloadedTrack(trackId);
+  }
+}
---
+++ flutter_client/lib/domain/usecases/download_track_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:frontend/data/repositories/track_repository.dart';
+import 'package:frontend/models/track.dart';
+
+class DownloadTrackUseCase {
+  final TrackRepository _repository;
+
+  DownloadTrackUseCase({TrackRepository? repository})
+      : _repository = repository ?? TrackRepository();
+
+  Future<String> execute(Track track) async {
+    return await _repository.downloadTrack(track);
+  }
+}
